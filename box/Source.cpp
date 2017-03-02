b658dd6ea1da8797835545650a4dc400ca2c0ff6
Farmak09 <farmak9@gmail.com>
2017-03-02 11:41:27 +0100

    moving box

--- box/Source.cpp
+++ box/Source.cpp
@@ -15,23 +15,68 @@ int main(int argc, char* args[])
 
 	SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
 	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
 	SDL_Rect r;
-	r.x = 215;
-	r.y = 125;
-	r.w = 200;
-	r.h = 200;
-
+	r.x = 0;
+	r.y = 0;
+	r.w = 50;
+	r.h = 50;
 	SDL_SetRenderDrawColor(renderer, 50, 50, 255, 255);
 	SDL_RenderFillRect(renderer, &r);
 	SDL_RenderPresent(renderer);
-
-	for (;;) 
+	
+	SDL_RenderPresent(renderer);
+	int x = 0;
+	int y = 0;
+	int i = 1;
+	while (i == 1) 
 	{
-
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_KEYDOWN)
+			{
+				switch (event.key.keysym.scancode)
+				{
+				case SDL_SCANCODE_UP:
+				{
+					y -= 5;
+					break;
+				}
+				case SDL_SCANCODE_DOWN:
+				{
+					y += 5;
+					break;
+				}
+				case SDL_SCANCODE_LEFT:
+				{
+					x -= 5;
+					break;
+				}
+				case SDL_SCANCODE_RIGHT:
+				{
+					x += 5;
+					break;
+				}
+				case SDL_SCANCODE_ESCAPE:
+				{
+					i = 0;
+					break;
+				}
+				default:
+				{
+					i = 1;
+				}
+				}		
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
+		SDL_RenderClear(renderer);
+		r.x = x;
+		r.y = y;
+		SDL_SetRenderDrawColor(renderer, 50, 50, 255, 255);
+		SDL_RenderFillRect(renderer, &r);
+		SDL_RenderPresent(renderer);
 	}
-
 	SDL_Quit();
 	return(EXIT_SUCCESS);
 }
